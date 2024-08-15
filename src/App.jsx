355d7ddee8a68f43da2cd3ac889f45328666822a
RuslanGon <ruslan441006@gmail.com>
2024-08-15 11:29:42 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import './App.css'
+import MailBox from './components/MailBox/MailBox.jsx'
 
 function App() {
  
 
   return (
-    <>
+    <div>
      <h1>Hello word</h1>
-    </>
+     <MailBox />
+    </div>
   )
 }
 
---
+++ src/components/MailBox/MailBox.jsx
@@ -0,0 +1,11 @@
+
+
+const MailBox = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default MailBox
