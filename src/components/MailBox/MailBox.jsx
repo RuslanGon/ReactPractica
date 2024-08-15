23f69399f40265626c43da2d27389ba142a0e6e0
RuslanGon <ruslan441006@gmail.com>
2024-08-15 15:30:38 +0300

    wrer

--- src/components/MailBox/MailBox.jsx
+++ src/components/MailBox/MailBox.jsx
@@ -1,9 +1,10 @@
 import MailBoxList from "../MailBoxList/MailBoxList.jsx";
+import css from '../MailBox/MailBox.module.css'
 
 const MailBox = ({ boxTitle, MailBoxCounter = 0, boxUser }) => {
   return (
-    <div>
-      <h2>{boxTitle}</h2>
+    <div className={css.maildiv}>
+      <h2 className={css.title}>{boxTitle}</h2>
       {MailBoxCounter === 0 ? (
         <p>
           <b>К сожалению, сейчас нет активных ячеек</b>
