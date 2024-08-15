b967fd9bc44c1cbe8cc6f028487207d0577ed4ff
RuslanGon <ruslan441006@gmail.com>
2024-08-15 11:52:02 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
   return (
     <div>
      <MailBox boxTitle="Meest Express" />   
+     <MailBox boxTitle="Nova Poshta" />   
+     <MailBox boxTitle="Ukr Poshta" />   
+
     </div>
   )
 }
