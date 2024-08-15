9f4f73349411efa65c9cf36cae826679b8b1cdd2
RuslanGon <ruslan441006@gmail.com>
2024-08-15 15:41:31 +0300

    wer

--- src/components/MailBoxItem/MailBoxItem.jsx
+++ src/components/MailBoxItem/MailBoxItem.jsx
@@ -4,7 +4,7 @@ import css from '../MailBoxItem/MailBoxItem.module.css'
 const MailBoxItem = ({user}) => {
   return (
     <li className={css.itemli} key={user.id}>
-            <p className={css.itemtext}>email:{user.userEmail}</p>
+            <p className={css.itemtext}>email: {user.userEmail}</p>
             <p className={css.itemtext}>name: {user.userName}</p>
           </li>
   )
