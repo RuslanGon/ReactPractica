359e80cd3e9b0f6c1822a786fd6e72041ee7a911
RuslanGon <ruslan441006@gmail.com>
2024-08-15 11:39:07 +0300

    wer

--- src/components/MailBox/MailBox.jsx
+++ src/components/MailBox/MailBox.jsx
@@ -4,6 +4,11 @@ const MailBox = () => {
   return (
     <div>
         <h2>MailBox Component</h2>
+        <ul>
+            <li>Ruslan@gmail.com</li>
+            <li>Lenan@gmail.com</li>
+            <li>Marina@gmail.com</li>
+        </ul>
     </div>
   )
 }
