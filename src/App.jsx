26f94382c829312a1de28bd768649f39dda5063a
RuslanGon <ruslan441006@gmail.com>
2024-08-16 15:12:32 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,14 @@ import UkrPoshta from '../ukr.json'
 
 
 function App() {
+
+  const handleClick = () => {
+
+  }
  
   return (
     <div>
+      <button onClick={handleClick}>Click me</button>
      <MailBox boxUser={MeestExpress} boxTitle="Meest Express" MailBoxCounter={5} />   
      <MailBox boxUser={NovaPoshta} boxTitle="Nova Poshta" MailBoxCounter={3} />   
      <MailBox boxUser={UkrPoshta} boxTitle="UKR Poshta" MailBoxCounter={0} />   
