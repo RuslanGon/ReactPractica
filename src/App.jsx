4c5f877a5b05319c3062af6f2df871f300e1342c
RuslanGon <ruslan441006@gmail.com>
2024-08-17 16:40:46 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -2,81 +2,12 @@
 import './App.css'
 import MailBox from './components/MailBox/MailBox.jsx'
 import MeestExpress from '../meest.json'
-import NovaPoshta from '../nova.json'
-import UkrPoshta from '../ukr.json'
-import DrinksCounter from './components/DrinksCounter/DrinksCounter.jsx'
-import DrinksValues from './components/DrinksValues/DrinksValues.jsx'
-import { useEffect, useState } from 'react'
-
-const initialValues = {beer: 0, whiskey: 0, wine: 0}
 
 function App() {
 
-  const [counter, setCounter] = useState(() => {
-    const counterSringify = localStorage.getItem('counterValues')
-    const parseCounter = JSON.parse(counterSringify) ?? 0
-    return parseCounter
-  })
-  const [isVisibleBar, setIsVisibleBar] = useState(false)
-  const [drinks, setDrinks] = useState(() => {
-    const strigifyDrinks =  localStorage.getItem('drinksValues')
-    const parseDrinks =  JSON.parse(strigifyDrinks) ?? initialValues
-    return parseDrinks
-  })
-
-  useEffect(() => {
-    localStorage.setItem('drinksValues', JSON.stringify(drinks))
-    }, [drinks])
-
-  useEffect(() => {
-    localStorage.setItem('counterValues', JSON.stringify(counter))
-    }, [counter])
-
-
-
-  const handleLogDrink = (drinkName) => {
-    if(drinks[drinkName] === 3 && drinkName === 'beer') return
-    setDrinks({...drinks, [drinkName] : drinks[drinkName] + 1})
-  };
-
-  const handleReset = () => {
-    setDrinks(initialValues);
-  }
-
-  const drinksTotal = drinks.beer + drinks.whiskey + drinks.wine
-  
-  const handleIncrementCounter = () => {
-    setCounter(counter + 1)
-  } 
-
-  const handleDecrementCounter = () => {
-    if(counter === 0) return
-    setCounter(counter - 1)
-  } 
-
-  const onToggleBarIsVisible = () => {
-    setIsVisibleBar(!isVisibleBar);
-  }
-
   return (
     <div>
-      {/* <MailBox boxUser={MeestExpress} boxTitle="Meest Express" MailBoxCounter={5} />   
-     <MailBox boxUser={NovaPoshta} boxTitle="Nova Poshta" MailBoxCounter={3} />   
-     <MailBox boxUser={UkrPoshta} boxTitle="UKR Poshta" MailBoxCounter={0} />   */}
-     <button onClick={onToggleBarIsVisible}>{isVisibleBar ? 'Hide' : 'Show'} mini-bar</button>
-      {isVisibleBar && (
-        <>
-          <button onClick={handleIncrementCounter}>Counter: {counter}</button>
-          <button onClick={handleDecrementCounter}>decrement</button>
-          <DrinksValues drinks={drinks} drinksTotal={drinksTotal} />
-          <DrinksCounter
-            handleLogDrink={handleLogDrink}
-            handleReset={handleReset}
-            onToggleBarIsVisible={onToggleBarIsVisible}
-            drinksTotal={drinksTotal}
-          />
-        </>
-      )}
+      <MailBox boxUser={MeestExpress} boxTitle="Meest Express" />   
     </div>
   );
 }
--- src/components/MailBox/MailBox.jsx
+++ src/components/MailBox/MailBox.jsx
@@ -1,23 +1,12 @@
 import MailBoxList from "../MailBoxList/MailBoxList.jsx";
 import css from '../MailBox/MailBox.module.css'
-import clsx from 'clsx'
 
-const MailBox = ({ boxTitle, MailBoxCounter = 0, boxUser }) => {
 
-  const isMailBoxCounterFull = MailBoxCounter === 0
+const MailBox = ({ boxTitle, boxUser }) => {
 
   return (
-    <div className={clsx(css.maildiv, {
-     [css.full] : isMailBoxCounterFull
-    })}>
+    <div className={css.maildiv}>
       <h2 className={css.title}>{boxTitle}</h2>
-      {MailBoxCounter === 0 ? (
-        <p className={css.text}>
-          <b>К сожалению, сейчас нет активных ячеек</b>
-        </p>
-      ) : (
-        <p className={css.text}>Количество активных ячеек: {MailBoxCounter}</p>
-      )}
       <MailBoxList boxUser={boxUser} />
     </div>
   );
