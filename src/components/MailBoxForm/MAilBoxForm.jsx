e13a6d164a6fb3c72a6a01fa5e7bd4a726b91014
RuslanGon <ruslan441006@gmail.com>
2024-08-18 10:50:42 +0300

    wer

--- src/components/MailBoxForm/MAilBoxForm.jsx
+++ src/components/MailBoxForm/MAilBoxForm.jsx
@@ -1,3 +1,4 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 
 const MAilBoxForm = ({onAddUsers}) => {
@@ -16,23 +17,26 @@ event.currentTarget.reset();
 }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Add new MailBox user</h2>
-      <label>
-        <span>User email:</span>
+    <Formik initialValues={{}} onSubmit={handleSubmit}>
+      <Form>
+        <h2>Add new MailBox user</h2>
+        <label>
+          <span>User email:</span>
+          <br />
+          <Field type="email" name="userEmail" placeholder="email" required />
+          <ErrorMessage name="userEmail" component="span" />
+        </label>
         <br />
-        <input type="email" name="userEmail" placeholder="email" required />
-      </label>
-      <br />
-      <label>
-        <span>User name:</span>
-        <br />
-        <input type="text" name="userName" placeholder="name" required />
-        <br />
-        <button type="submit">Create new user 👱</button>
-      </label>
-    </form>
-    
+        <label>
+          <span>User name:</span>
+          <br />
+          <Field type="text" name="userName" placeholder="name" required />
+          <ErrorMessage name="userName" component="span" />
+          <br />
+          <button type="submit">Create new user 👱</button>
+        </label>
+      </Form>
+    </Formik>
   );
 };
 
