12298caebd1cdf9d17fde5450131d72ce753f4c9
RuslanGon <ruslan441006@gmail.com>
2024-08-18 14:42:55 +0300

    wrr

--- src/AppWithHttpRequest.jsx
+++ src/AppWithHttpRequest.jsx
@@ -1,9 +1,31 @@
 
 import './App.css'
-
+    // "id": 1,
+    // "products": [
+    //   {
+    //     "id": 168,
+    //     "title": "Charger SXT RWD",
+    //     "price": 32999.99,
+    //     "quantity": 3,
+    //     "total": 98999.97,
+    //     "discountPercentage": 13.39,
+    //     "discountedTotal": 85743.87,
+    //     "thumbnail": "https://cdn.dummyjson.com/products/images/vehicle/Charger%20SXT%20RWD/thumbnail.png"
 const AppWithHttpRequest = () => {
   return (
-    <div>AppWithHttpRequest</div>
+    <div>
+        <h1>Smart Ukrainian Big Product Store</h1>
+        <ul>
+            <li>
+                <img width={250} height={250} src="" alt="" />
+                <h2>Title:</h2>
+                <p>Description:</p>
+                <p>Id:</p>
+                <p>Total:</p>
+                <h3>Price:</h3>
+            </li>
+        </ul>
+    </div>
   )
 }
 
