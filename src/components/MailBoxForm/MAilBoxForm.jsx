222259cf578428db340aafce16d140c69309ad00
RuslanGon <ruslan441006@gmail.com>
2024-08-18 11:22:46 +0300

    wer

--- src/components/MailBoxForm/MAilBoxForm.jsx
+++ src/components/MailBoxForm/MAilBoxForm.jsx
@@ -1,10 +1,13 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as yup from "yup";
+import * as Yup from "yup";
 
-const mailBoxSchema = yup.object({
-  userEmail: yup.string().defined(),
-  userName: yup.string().default("").nullable(),
+
+const mailBoxSchema = Yup.object({
+  userEmail: Yup.string()
+    .email("Invalid email format").required("Email is required"),
+  userName: Yup.string().required("Name is required"),
 });
+
 
 const FORM_INITIAL_VALUES = {
   userEmail: "",
@@ -18,21 +21,24 @@ const MAilBoxForm = ({ onAddUsers }) => {
   };
 
   return (
-    <Formik initialValues={FORM_INITIAL_VALUES} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={FORM_INITIAL_VALUES}
+      onSubmit={handleSubmit}
       validationSchema={mailBoxSchema}
+    >
       <Form>
         <h2>Add new MailBox user</h2>
         <label>
           <span>User email:</span>
           <br />
-          <Field type="email" name="userEmail" placeholder="email" required />
+          <Field type="email" name="userEmail" placeholder="email" />
           <ErrorMessage name="userEmail" component="span" />
         </label>
         <br />
         <label>
           <span>User name:</span>
           <br />
-          <Field type="text" name="userName" placeholder="name" required />
+          <Field type="text" name="userName" placeholder="name" />
           <ErrorMessage name="userName" component="span" />
           <br />
           <button type="submit">Create new user 👱</button>
