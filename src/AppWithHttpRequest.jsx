c2024c439265bf5acb1ed1aff1a39d1ef866d6d1
RuslanGon <ruslan441006@gmail.com>
2024-08-18 15:51:28 +0300

    wer

--- src/AppWithHttpRequest.jsx
+++ src/AppWithHttpRequest.jsx
@@ -22,8 +22,8 @@ const AppWithHttpRequest = () => {
             <li key={product.id}>
               <img width={250} height={250} src={product.thumbnail} alt={product.title} />
               <h2>Title: {product.title}</h2>
-              <p>Id: {product.id}</p>
-              <p>Total: {product.total}</p>
+              <p>Rating: {product.rating}</p>
+              <p>Category: {product.category}</p>
               <h3>Price: {product.price}</h3>
             </li>
           ))
