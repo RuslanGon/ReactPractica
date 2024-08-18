a62a73fa315c60d82aaa52f9d16b4143ddea4010
RuslanGon <ruslan441006@gmail.com>
2024-08-18 16:05:05 +0300

    wer

--- src/AppWithHttpRequest.jsx
+++ src/AppWithHttpRequest.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import Loader from './components/Loader/Loader.jsx';
 
 const AppWithHttpRequest = () => {
   const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     async function fetchProducts() {
+      setIsLoading(true)
       const { data } = await axios.get("https://dummyjson.com/products");
       setProducts(data.products);
+      setIsLoading(false)
     }
     fetchProducts();
   }, []);
@@ -16,6 +20,8 @@ const AppWithHttpRequest = () => {
   return (
     <div>
       <h1>Smart Ukrainian Big Product Store</h1>
+      {isLoading && <Loader />
+      }
       <ul>
         {Array.isArray(products) &&
           products.map((product) => (
