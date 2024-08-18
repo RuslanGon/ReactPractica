f9b0602444445c8f4a9f7665a98d28c11b2153ea
RuslanGon <ruslan441006@gmail.com>
2024-08-18 16:40:10 +0300

    wer

--- src/AppWithHttpRequest.jsx
+++ src/AppWithHttpRequest.jsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Loader from './components/Loader/Loader.jsx';
-import { Error } from './components/Error/Error.jsx';
-import { requestProducts } from './services/api.js';
-import ProductList from './components/ProductList/ProductList.jsx';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Loader from "./components/Loader/Loader.jsx";
+import { Error } from "./components/Error/Error.jsx";
+import { requestProducts } from "./services/api.js";
+import ProductList from "./components/ProductList/ProductList.jsx";
 
 const AppWithHttpRequest = () => {
   const [products, setProducts] = useState(null);
-  const [isLoading, setIsLoading] = useState(false)
-  const [isError, setIsError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     async function fetchProducts() {
       try {
         setIsLoading(true);
-        const  data  = await requestProducts() ;
+        const data = await requestProducts();
         setProducts(data.products);
       } catch {
         setIsError(true);
@@ -29,8 +29,8 @@ const AppWithHttpRequest = () => {
     <div>
       <h1>Smart Ukrainian Big Product Store</h1>
       {isLoading && <Loader />}
-      {isError && <Error /> }
-      <ProductList products={products} />
+      {isError && <Error />}
+      {products && <ProductList products={products} />}
     </div>
   );
 };
