5ba6b1c2ec05ca17e4da9ea8688c6c87b724e142
RuslanGon <ruslan441006@gmail.com>
2024-08-19 16:08:34 +0300

    wer

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,4 +1,5 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
+// import { useEffect, useRef } from "react";
 import * as Yup from "yup";
 
 
@@ -12,6 +13,14 @@ const FORM_INITIAL_VALUES = {
 };
 
 const SearchForm = ({ onSetSearchQuery }) => {
+
+  // const inputRef = useRef(null);
+  // useEffect(() => {
+  //   if (inputRef.current) {
+  //     inputRef.current.focus(); // Устанавливаем фокус на инпуте при загрузке страницы
+  //   }
+  // }, []);
+
   const handleSubmit = (values) => {
     // console.log(values);
     onSetSearchQuery(values.searchTerm);
@@ -29,7 +38,6 @@ const SearchForm = ({ onSetSearchQuery }) => {
           <Field type="text" name="searchTerm" placeholder="search" />
           <ErrorMessage name="searchTerm" component="span" />
         </label>
-    
       <button type="submit" aria-label="Search">🔎</button>
       </Form>
     </Formik>
