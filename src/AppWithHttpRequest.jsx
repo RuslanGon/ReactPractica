6122a8245e3a054615af3802159787e75cdbdf2b
RuslanGon <ruslan441006@gmail.com>
2024-08-19 15:10:46 +0300

    wer

--- src/AppWithHttpRequest.jsx
+++ src/AppWithHttpRequest.jsx
@@ -1,53 +1,15 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import "./App.css";
 import Loader from "./components/Loader/Loader.jsx";
 import { Error } from "./components/Error/Error.jsx";
-import { requestProducts, requestProductsByQuery } from "./services/api.js";
+// import { requestProducts, requestProductsByQuery } from "./services/api.js";
 import ProductList from "./components/ProductList/ProductList.jsx";
 import SearchForm from "./components/SearchForm/SearchForm.jsx";
+import { useProductSearch } from "./hooks/useProductSearch.jsx";
 
 const AppWithHttpRequest = () => {
-  const [products, setProducts] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [query, setQuery] = useState('')
-
-  useEffect(() => {
-    async function fetchProducts() {
-      try {
-        setIsLoading(true);
-        const data = await requestProducts();
-        setProducts(data.products);
-      } catch {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchProducts();
-  }, []);
-
-
-useEffect(() => {
-if(query.length === 0) return
-
-async function fetchProductsByQuery() {
-  try {
-    setIsLoading(true);
-    const data = await requestProductsByQuery(query);
-    setProducts(data.products);
-  } catch {
-    setIsError(true);
-  } finally {
-    setIsLoading(false);
-  }
-}
-fetchProductsByQuery();
-}, [query])
-
-  const onSetSearchQuery = (searchTerm) => {
-    setQuery(searchTerm);
-  };
+ 
+   const { products, isLoading, isError, onSetSearchQuery} = useProductSearch();
 
   return (
     <div>
