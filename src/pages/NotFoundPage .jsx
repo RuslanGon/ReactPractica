db0d68667c6a09b7c77d069221f6e77adde095a9
RuslanGon <ruslan441006@gmail.com>
2024-08-20 11:55:47 +0300

    wer

--- src/pages/NotFoundPage .jsx
+++ src/pages/NotFoundPage .jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NotFoundPage = () => {
-    return (
-      <div style={{ textAlign: 'center', marginTop: '50px' }}>
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-      </div>
-    );
-  };
-  
-  export default NotFoundPage;
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <Link to="/">Go home</Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
