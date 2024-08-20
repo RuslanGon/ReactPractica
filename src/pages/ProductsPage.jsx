b273c52732972db80fb485ef446f5425f6d74ca8
RuslanGon <ruslan441006@gmail.com>
2024-08-20 12:31:16 +0300

    wer

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -3,17 +3,15 @@ import "../App.css";
 import Loader from "../components/Loader/Loader.jsx";
 import { Error } from "../components/Error/Error.jsx";
 import ProductList from "../components/ProductList/ProductList.jsx";
-import SearchForm from "../components/SearchForm/SearchForm.jsx";
 import { useProductSearch } from "../hooks/useProductSearch.jsx";
 
 const ProductsPage = () => {
  
-   const { products, isLoading, isError, onSetSearchQuery} = useProductSearch();
+   const { products, isLoading, isError} = useProductSearch();
 
   return (
     <div>
       <h1>Smart Ukrainian Big Product Store</h1>
-      <SearchForm onSetSearchQuery={onSetSearchQuery} />
       {isLoading && <Loader />}
       {isError && <Error />}
       {products && <ProductList products={products} />}
