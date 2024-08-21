c2271a4c99033a16f74d23be75da45ffc99e58c6
RuslanGon <ruslan441006@gmail.com>
2024-08-21 14:43:54 +0300

    wer

--- src/hooks/useProductSearch.jsx
+++ src/hooks/useProductSearch.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import { requestProducts, requestProductsByQuery } from "../services/api.js";
+import { useSearchParams } from "react-router-dom";
 
 export const useProductSearch = ({isSearchPage = false}) => {
 
     const [products, setProducts] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
-    const [query, setQuery] = useState('')
+    // const [query, setQuery] = useState('')
+
+    const [searchParams, setSearchParams] = useSearchParams()
+    const query = searchParams.get('query')
   
     useEffect(() => {
       if(isSearchPage) return
@@ -26,7 +30,7 @@ export const useProductSearch = ({isSearchPage = false}) => {
   
   
   useEffect(() => {
-  if(query.length === 0) return
+  if(query === null) return
   
   async function fetchProductsByQuery() {
     try {
@@ -43,7 +47,8 @@ export const useProductSearch = ({isSearchPage = false}) => {
   }, [query])
   
     const onSetSearchQuery = (searchTerm) => {
-      setQuery(searchTerm);
+      // setQuery(searchTerm);
+      setSearchParams({query: searchTerm})
     };
 
   return { products, isLoading, isError, onSetSearchQuery }
