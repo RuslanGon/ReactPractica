1690e587883c9ace3e1453cc23a4b820949ead1a
RuslanGon <ruslan441006@gmail.com>
2024-08-21 15:21:43 +0300

    wer

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,60 +1,60 @@
-import { useEffect, useRef, useState } from "react"
-import { Link, Route, Routes, useLocation, useParams } from "react-router-dom"
-import Loader from "../components/Loader/Loader.jsx"
-import { Error } from "../components/Error/Error.jsx"
-import { requestProductsDetailsById } from "../services/api.js"
-import CommentPage from "./CommentPage.jsx"
-import ReviewsPage from "./ReviewsPage.jsx"
-
+import { useEffect, useRef, useState } from "react";
+import { Link, Route, Routes, useLocation, useParams } from "react-router-dom";
+import Loader from "../components/Loader/Loader.jsx";
+import { Error } from "../components/Error/Error.jsx";
+import { requestProductsDetailsById } from "../services/api.js";
+import CommentPage from "./CommentPage.jsx";
+import ReviewsPage from "./ReviewsPage.jsx";
 
 const ProductDetailsPage = () => {
+  const location = useLocation();
+  const backRefLink = useRef(location.state ?? "/");
 
-    const location = useLocation()
-    const backRefLink = useRef(location.state ?? '/')
+  const { productId } = useParams();
 
-    const {productId} = useParams()
+  const [productDetails, setProductDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-    const [productDetails, setProductDetails] = useState(null)
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
-
-    useEffect(() => {
-
-        async function fetchProductsDetails () {
-            try{
-                setIsLoading(true);
-                const data = await requestProductsDetailsById(productId);
-                setProductDetails(data)
-            }catch{
-                setIsError(true);
-            }finally{
-                setIsLoading(false);
-            }
-        }
-        fetchProductsDetails()
-
-    }, [productId])
+  useEffect(() => {
+    async function fetchProductsDetails() {
+      try {
+        setIsLoading(true);
+        const data = await requestProductsDetailsById(productId);
+        setProductDetails(data);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchProductsDetails();
+  }, [productId]);
 
   return (
     <div>
-      <Link to={backRefLink.current}> Go back</Link>
+      <Link style={{ fontSize: "20px", color: "black"}} to={backRefLink.current}>
+        Go <span style={{ fontSize: "30px" }}>🔙</span>
+      </Link>
       {isLoading && <Loader />}
       {isError && <Error />}
-        <h1>Product Details {productId}</h1>
-       {productDetails !== null && <div>
+      <h1>Product Details {productId}</h1>
+      {productDetails !== null && (
+        <div>
           <img src={productDetails.thumbnail} alt={productDetails.title} />
           <h2>Title: {productDetails.title}</h2>
           <p>Brand: {productDetails.brand}</p>
           <p>Price: ${productDetails.price}</p>
-        </div>}
-        <Link to='comments'>Comment</Link>
-        <Link to='reviews'>Reviews</Link>
-        <Routes>
-        <Route path="comments" element={<CommentPage />} />  
-        <Route path="reviews" element={<ReviewsPage />} />  
-        </Routes>
+        </div>
+      )}
+      <Link to="comments">Comment</Link>
+      <Link to="reviews">Reviews</Link>
+      <Routes>
+        <Route path="comments" element={<CommentPage />} />
+        <Route path="reviews" element={<ReviewsPage />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetailsPage
+export default ProductDetailsPage;
