ec87e5edf48fe2fef702db9480cd55c15dfee4fd
RuslanGon <ruslan441006@gmail.com>
2024-08-22 18:00:15 +0300

    wer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 
 import { mailboxReducer } from "./mailbox/mailboxReduser.js";
 import { configureStore } from "@reduxjs/toolkit";
+import { timerReducer } from "./timer/timerSlice.js";
 
 
 export const store = configureStore({
     reducer: {
-        mailbox: mailboxReducer 
+        mailbox: mailboxReducer ,
+        timer: timerReducer
     }
   });
   
---
+++ src/redux/timer/timerSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+ timer: 0
+};
+
+const timerSlice = createSlice({
+  // Ім'я слайсу
+  name: "timer",
+  // Початковий стан редюсера слайсу
+  initialState: initialState,
+  // Об'єкт редюсерів
+  reducers: {
+   incrementTimer(state, action) {state.timer += action.payload || 1 },
+  },
+});
+
+// Генератори екшенів
+export const { incrementTimer} = timerSlice.actions;
+
+// Редюсер слайсу
+export const timerReducer = timerSlice.reducer;
+
