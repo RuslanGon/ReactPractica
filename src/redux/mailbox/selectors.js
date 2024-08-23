97c7e2fa112186f4496f9979f15efee482246df8
RuslanGon <ruslan441006@gmail.com>
2024-08-23 14:26:14 +0300

    wer

--- src/redux/mailbox/selectors.js
+++ src/redux/mailbox/selectors.js
@@ -1,3 +1,15 @@
+import { createSelector } from "@reduxjs/toolkit"
+
 export const selectorUsers = state => state.mailbox.users
 export const selectFilter = state => state.mailbox.filter
+
+export const selectFiltetredUsers = createSelector(
+    [selectorUsers, selectFilter],
+    (users, filter) => {
+        return users.filter(user =>
+            user.userName.toLowerCase().includes(filter.toLowerCase()) ||
+            user.userEmail.toLowerCase().includes(filter.toLowerCase())
+          )
+    }
+)
 
