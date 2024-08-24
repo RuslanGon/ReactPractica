cec44d60f5f8a303dc7c7b52edc6c78c8d8a0aac
RuslanGon <ruslan441006@gmail.com>
2024-08-24 12:03:03 +0300

    wer

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+import { apiRegistor } from "./operation.js";
 
 export const instance = axios.create({
     baseURL: 'https://connections-api.goit.global/docs/#/User/post_users_signup'
@@ -26,6 +27,23 @@ const authSlice = createSlice({
   name: "auth",
   // Початковий стан редюсера слайсу
   initialState: initialState,
+
+  extraReducers: (builder) =>
+    builder.
+    addCase(apiRegistor.pending, (state) => {
+      state.isLoading = true, 
+      state.isError = false;
+    })
+    .addCase(apiRegistor.fulfilled, (state, action) => {
+        state.isLoading = false, 
+        state.isSignedIn = true,
+        state.userData = action.payload.user
+        state.token = action.payload.token
+    })
+    .addCase(apiRegistor.rejected, (state) => {
+        state.isLoading = false, 
+        state.isError = true;
+    })
 });
 
 // Редюсер слайсу
--- src/redux/auth/operation.js
+++ src/redux/auth/operation.js
@@ -1,11 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { instance } from "./authSlice.js";
+import { instance, setToken } from "./authSlice.js";
 
 export const apiRegistor = createAsyncThunk(
     "auth/register",
     async (formData, thunkApi) => {
       try {
         const {data} = await instance.post('/users/signup', formData);
+        setToken(data.token)
         return data;
       } catch (error) {
         return thunkApi.rejectWithValue(error.message);
