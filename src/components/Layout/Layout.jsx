c50a967578fb8c912cf8be327c55bc04732301c3
RuslanGon <ruslan441006@gmail.com>
2024-08-25 10:12:46 +0300

    wer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,49 +2,64 @@ import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 import css from "../../App.module.css";
 import { useSelector } from "react-redux";
-import { selectIsSignedIn } from "../../redux/auth/selectors.js";
-
+import {
+  selectIsSignedIn,
+  selectUserData,
+} from "../../redux/auth/selectors.js";
 
 const getNavLinkClassName = ({ isActive }) => {
-    return clsx(css.navLink, {
-      [css.active]: isActive,
-    });
-  };
+  return clsx(css.navLink, {
+    [css.active]: isActive,
+  });
+};
 const Layout = ({ children }) => {
+  const isSignedIn = useSelector(selectIsSignedIn);
 
-    const isSignedIn = useSelector(selectIsSignedIn)
+  const userData = useSelector(selectUserData);
 
   return (
     <div>
-         <header>
+      <header>
         <nav className={css.nav}>
           <NavLink className={getNavLinkClassName} to="/">
             Home Page
           </NavLink>
-          { isSignedIn ? <> <NavLink className={getNavLinkClassName} to="/contacts">
-            Contacts Page
-          </NavLink>
-          <NavLink className={getNavLinkClassName} to="/mailbox">
-            MailBox
-          </NavLink>
-          <NavLink className={getNavLinkClassName} to="/products">
-            Products
-          </NavLink>
-          <NavLink className={getNavLinkClassName} to="/search">
-            Search
-          </NavLink> </> : <> <NavLink className={getNavLinkClassName} to="/register">
-            Registr Page
-          </NavLink>
-          <NavLink className={getNavLinkClassName} to="/login">
-            Login Page
-          </NavLink> </> }
-          
+          {isSignedIn ? (
+            <>
+              <NavLink className={getNavLinkClassName} to="/contacts">
+                Contacts Page
+              </NavLink>
+              <NavLink className={getNavLinkClassName} to="/mailbox">
+                MailBox
+              </NavLink>
+              <NavLink className={getNavLinkClassName} to="/products">
+                Products
+              </NavLink>
+              <NavLink className={getNavLinkClassName} to="/search">
+                Search
+              </NavLink>
+
+              <div>
+               <span> Helloy {userData}</span>
+               <button type="button">Log out 🏃</button>
+              </div>
+
+            </>
+          ) : (
+            <>
+              <NavLink className={getNavLinkClassName} to="/register">
+                Registr Page
+              </NavLink>
+              <NavLink className={getNavLinkClassName} to="/login">
+                Login Page
+              </NavLink>
+            </>
+          )}
         </nav>
         <main>{children}</main>
       </header>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,11 @@
+// import { Helmet } from "react-helmet-async";
+
 const HomePage = () => {
     return (
       <div style={{ textAlign: 'center', padding: '20px' }}>
+        {/* <Helmet>
+          <title>Home page for FS98</title>
+        </Helmet> */}
         <h1>Welcome to the Home Page 📃</h1>
         <p>This is the main landing page of our application. Here you can find links to various sections of our website.</p>
       </div>
